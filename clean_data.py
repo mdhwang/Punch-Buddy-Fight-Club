b007984ee1d50cbe23e1c643d0243f96b0d11d83
mdhwang <mattdhwang@gmail.com>
2019-11-09 21:45:21 -0800

    updated cleaning py

--- clean_data.py
+++ clean_data.py
@@ -9,6 +9,8 @@ import glob
 def process_staging():
     # Processes all new data in staging folder
     # Spits out to processed folder
+
+    print("Process Staging Data Begin")
     path = '/Users/matthewhwang/Galvanize/fightclub/data/staging'
     folders = []
     
@@ -28,10 +30,12 @@ def process_staging():
         count = int(name[2])
         user = name[3]
         known_clean_data(accel_data,gyro_data,count,8,category,user)
+        print("Did one")
+    print("Finished")
     pass
 
 
-def known_clean_data(input_data_accel,input_data_gyro,num_punches,window,category,user):
+def known_clean_data(accel_data,gyro_data,num_punches,window,category,user):
 
     # Initial cleaning of data from raw accel to ID events.
     # Input data as Accel and Gyro data from Mbeint Labs Sensor App
@@ -41,12 +45,12 @@ def known_clean_data(input_data_accel,input_data_gyro,num_punches,window,categor
 
     # INPUTS
     # -------------------------------------------------------------------
-    # input_data_accel - pandas df
-    # input_data_gyro - pandas df
+    # accel_data - pandas df
+    # gyro_data - pandas df
     # num_punches - int - prescribed number of punches per this trial
     # window - int - how many data points before and after spike occurs
     # category - str - what category of punch (jab, left hook, etc.)
-
+    # user - who dun it
 
     # OUTPUT
     # -------------------------------------------------------------------
@@ -56,13 +60,13 @@ def known_clean_data(input_data_accel,input_data_gyro,num_punches,window,categor
 
     # drop extraneous columns
     drop_cols = ['epoch (ms)','time (-08:00)']
-    input_data_accel = input_data_accel.drop(drop_cols,axis=1)
-    input_data_gyro = input_data_gyro.drop(drop_cols,axis=1)
-    input_data_gyro = input_data_gyro.drop(['elapsed (s)'],axis=1)
+    accel_data = accel_data.drop(drop_cols,axis=1)
+    gyro_data = gyro_data.drop(drop_cols,axis=1)
+    gyro_data = gyro_data.drop(['elapsed (s)'],axis=1)
     
 
     # join into single data set
-    combined = input_data_accel.merge(input_data_gyro,how="outer", left_index=True, right_index=True)
+    combined = accel_data.merge(gyro_data,how="outer", left_index=True, right_index=True)
 
     # calculate magnitude col to ID top events
     combined["magnitude"] = np.sqrt(combined["X-Axis (g)"]**2+combined["Y-Axis (g)"]**2+combined["Z-Axis (g)"]**2)
@@ -70,21 +74,22 @@ def known_clean_data(input_data_accel,input_data_gyro,num_punches,window,categor
     # determine row indicies of num_punches number of events
     event_index = sorted(combined.sort_values(["magnitude"],ascending=False)["elapsed (s)"].head(num_punches).to_dict())
 
+    # magnitude unnecessary once used to ID location of events
+    combined = combined.drop(["elapsed (s)","magnitude"],axis=1)
+
+    combined['category'] = category
+
     # find event and store window of data surrounding it
     punch_data = []
     for each in event_index:
-        data_pts = window * 2 + 1
-        dummy = combined.iloc[each-window:each+window+1]
-        dummy["time step"] = list(np.linspace(0 , data_pts * 0.04 , data_pts))
-        dummy['target'] = category
-        dummy = dummy.drop(['elapsed (s)'],axis=1)
-        punch_data.append(dummy)
+        punch_data.append(combined.iloc[each-window:each+window+1])
     
     data_obj = {}
     data_obj['params'] = {}
     data_obj['params']['category'] = category
     data_obj['params']['num_punches'] = num_punches
     data_obj['params']['window'] = window
+    data_obj['params']['fighter'] = user
     data_obj['data'] = punch_data
     
     current_time = datetime.now().strftime("%m.%d.%Y, %H.%M.%S")
